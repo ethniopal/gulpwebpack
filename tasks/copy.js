4d96d2943ba5e2e6b1188d3ac1a70116b0df1ad9
Olivier Lapointe <olivier.lapointe87@gmail.com>
2019-10-02 15:39:46 -0400

    Correction de la copie des fichiers lorsque c'était au niveau des plugins

--- tasks/copy.js
+++ tasks/copy.js
@@ -6,10 +6,10 @@ import notify    from 'gulp-notify' //afficher notification
 
 function copy() {
     return src([
-        config.theme.src +'/**/*',
-        '!'  + config.plugins.src + '/**/*',
-        '!'  + config.muplugins.src + '/**/*',
-        '!' + config.theme.src + '/{js,js/**,css,css/**,scss,scss/**,img,img/**}'], //Fichier et répertoire à ne pas copier
+            config.theme.src +'/**/*',
+            '!'  + config.theme.src + '/plugins/**/*',
+            '!'  + config.theme.src + '/mu-plugins/**/*',
+            '!' + config.theme.src + '/{js,js/**,css,css/**,scss,scss/**,img,img/**}'], //Fichier et répertoire à ne pas copier
         {allowEmpty: true, since: lastRun(copy) })
 
         .pipe(dest(config.theme.dist))
@@ -17,16 +17,19 @@ function copy() {
 
 }
 
-function copyPlugins() {
-    return src(
-        config.plugins.src +'/**/*',   {allowEmpty: true, since: lastRun(copyPlugins) })
-        .pipe(dest(config.plugins.dist))
+function copyPlugins(done) {
+    if(config.type=='wp') {
+        return src(
+            config.plugins.src + '/**/*', {allowEmpty: true, since: lastRun(copyPlugins)})
+            .pipe(dest(config.plugins.dist))
 
-        .pipe(src(
-            config.muplugins.src +'/**/*', {allowEmpty: true, since: lastRun(copyPlugins) })
-        )
-        .pipe(dest(config.muplugins.dist))
-        .pipe(notify({message: 'TASK: Copy completed! 💯', onLast: true}));
+            .pipe(src(
+                config.muplugins.src + '/**/*', {allowEmpty: true, since: lastRun(copyPlugins)})
+            )
+            .pipe(dest(config.muplugins.dist))
+            .pipe(notify({message: 'TASK: Copy completed! 💯', onLast: true}));
+    }
+    done();
 }
 
 
